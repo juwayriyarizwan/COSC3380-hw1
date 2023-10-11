27e9ea3cd08f2fad709524918fe312e4197de481
michaelnguyen567 <116745115+michaelnguyen567@users.noreply.github.com>
2023-10-10 21:43:21 -0500

    Add files via upload

--- normalize.py
+++ normalize.py
@@ -89,92 +89,99 @@ try:
     # Open text file 
     sys.stdout = open("nf.txt", "a+")  
     
-    
-            
-    # Validate the given primary key
-    cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {joinPk} HAVING COUNT(*) > 1);")
-    checkDuplicate = cursor.fetchone()[0]
-    # Checks if there exists duplicate values in primary key with 1 attribute
-    if len(tablePk) == 1:
-        if checkDuplicate == True:
-            print(f"PK\tinvalid") # There are duplicates
-        else:
-            print(f"PK\tvalid") # There are no duplicates
-            validPk = True
+    cursor.execute(f"SELECT COUNT(*) FROM db.{tableName};")
+    numRows = cursor.fetchone()[0]
+    if numRows == 0 or numRows == 1:
+        print(f"PK\tvalid")
+        print(f"1NF\tY")
+        print(f"2NF\tY")
+        print(f"3NF\tY")
+        print(f"BCNF\tY")
     else:
-        # For each individual attribute in a composite key, checks if duplicates exist
-        validPk = True
-        for pk in tablePk:
-            cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {pk} HAVING COUNT(*) > 1);")
-            checkDuplicates = cursor.fetchone()[0]
-            # If there are no duplicates, the primary key attribute is a partial dependency
-            if checkDuplicates == False:
-                validPk = False
-
-        # Checks if full composite key has duplicates
+        # Validate the given primary key
         cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {joinPk} HAVING COUNT(*) > 1);")
         checkDuplicate = cursor.fetchone()[0]
-        if checkDuplicate == True:
-            validPk = False # There are no duplicates
-        
-        if validPk == True:
-            print(f"PK\tvalid") # Attributes fully dependant on full key
+        # Checks if there exists duplicate values in primary key with 1 attribute
+        if len(tablePk) == 1:
+            if checkDuplicate == True:
+                print(f"PK\tinvalid") # There are duplicates
+            else:
+                print(f"PK\tvalid") # There are no duplicates
+                validPk = True
         else:
-            print(f"PK\tinvalid") # Partial dependency, not minimal
-
-        
-    # 1NF Check
-    # Checks if there exists duplicate rows
-    cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {joinAll} HAVING COUNT(*) > 1);")
-    checkDuplicate = cursor.fetchone()[0]
-    if checkDuplicate == True:
-        print(f"1NF\tN") # There are duplicates
-    else:
-        print(f"1NF\tY") # There are no duplicates
-        currentForm = "1NF"
-        
-    # 2NF Check
-    if validPk == True and currentForm == "1NF":
-        currentForm = "2NF"
-        print(f"2NF\tY")
-    else:
-        print(f"2NF\tN")
-    
-    # 3NF Check
-    # Checks for dependencies from non-key attributes
-    checkTransitiveDepend = False
-    if currentForm == "2NF":
-        for npk in set(tableCol) - set(tablePk):
-            for col in tableCol:
-                for pk in tablePk:
-                    cursor.execute(f"SELECT EXISTS (SELECT 1 FROM {tableName} t1 WHERE NOT EXISTS (SELECT 1 FROM {tableName} t2 WHERE t2.{pk} = t1.{pk} AND t2.{col} <> t1.{col})) AS transitive_dependency;")
-                    transitive_dependency = cursor.fetchone()[0]
-                    if transitive_dependency:
-                        checkTransitiveDepend = True
-                        break
-    if checkTransitiveDepend:
-        print(f"3NF\t\tN")  # There are transitive dependencies 
-    else:
-        print(f"3NF\t\tY")  # There are no transitive dependencies
-        currentForm = "3NF"
-
-    # BCNF Check
-    # Checks for dependencies from non-key attributes
-    checkDependency = False
-    if currentForm == "3NF":
-        for npk in set(tableCol) - set(tablePk):
+            # For each individual attribute in a composite key, checks if duplicates exist
+            validPk = True
             for pk in tablePk:
-                cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM {tableName} GROUP BY {pk}, {npk} HAVING COUNT(*) > 1);")
-                if cursor.fetchone()[0]:
-                    checkDependency = True
-                    break
-            if checkDependency:
-                break 
-    if checkDependency:
-        print(f"BCNF\tN")  # There are transitive dependencies
-    else:
-        print(f"BCNF\tY")  # There are no transitive dependencies
-        currentForm = "BCNF" 
+                cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {pk} HAVING COUNT(*) > 1);")
+                checkDuplicates = cursor.fetchone()[0]
+                # If there are no duplicates, the primary key attribute is a partial dependency
+                if checkDuplicates == False:
+                    validPk = False
+
+            # Checks if full composite key has duplicates
+            cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {joinPk} HAVING COUNT(*) > 1);")
+            checkDuplicate = cursor.fetchone()[0]
+            if checkDuplicate == True:
+                validPk = False # There are no duplicates
+        
+            if validPk == True:
+                print(f"PK\tvalid") # Attributes fully dependant on full key
+            else:
+                print(f"PK\tinvalid") # Partial dependency, not minimal
+
+        
+        # 1NF Check
+        # Checks if there exists duplicate rows
+        cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {joinAll} HAVING COUNT(*) > 1);")
+        checkDuplicate = cursor.fetchone()[0]
+        if checkDuplicate == True:
+            print(f"1NF\tN") # There are duplicates
+        else:
+            print(f"1NF\tY") # There are no duplicates
+            currentForm = "1NF"
+        
+        # 2NF Check
+        if validPk == True and currentForm == "1NF":
+            currentForm = "2NF"
+            print(f"2NF\tY")
+        else:
+            print(f"2NF\tN")
+    
+        # 3NF Check
+        # Checks for dependencies from non-key attributes
+        checkTransitiveDepend = False
+        if currentForm == "2NF":
+            for npk in set(tableCol) - set(tablePk):
+                for col in tableCol:
+                    for pk in tablePk:
+                        cursor.execute(f"SELECT EXISTS (SELECT 1 FROM db.{tableName} t1 WHERE NOT EXISTS (SELECT 1 FROM {tableName} t2 WHERE t2.{pk} = t1.{pk} AND t2.{col} <> t1.{col})) AS transitive_dependency;")
+                        transitive_dependency = cursor.fetchone()[0]
+                        if transitive_dependency:
+                            checkTransitiveDepend = True
+                            break
+        if checkTransitiveDepend:
+            print(f"3NF\t\tN")  # There are transitive dependencies 
+        else:
+            print(f"3NF\t\tY")  # There are no transitive dependencies
+            currentForm = "3NF"
+
+        # BCNF Check
+        # Checks for dependencies from non-key attributes
+        checkDependency = False
+        if currentForm == "3NF":
+            for npk in set(tableCol) - set(tablePk):
+                for pk in tablePk:
+                    cursor.execute(f"SELECT EXISTS (SELECT COUNT(*) FROM db.{tableName} GROUP BY {pk}, {npk} HAVING COUNT(*) > 1);")
+                    if cursor.fetchone()[0]:
+                        checkDependency = True
+                        break
+                if checkDependency:
+                    break 
+        if checkDependency:
+            print(f"BCNF\tN")  # There are transitive dependencies
+        else:
+            print(f"BCNF\tY")  # There are no transitive dependencies
+            currentForm = "BCNF" 
 
 
 # Print any errors that occured trying to establish connection or execute a query
